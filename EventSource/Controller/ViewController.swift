8ff0c588304273348e0ccf3d2d9bc4f32f938445
shashankprabhakar <shashank.dwayne@gmail.com>
2019-10-04 21:39:52 +0100

    Revert "deploy on firebase and add the login story board at first"
    This reverts commit 9159d1722fb7aa2607d4ed60ae63e824a3ae74c5.

--- EventSource/Controller/ViewController.swift
+++ EventSource/Controller/ViewController.swift
@@ -13,14 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-       
     }
 
-    override func viewDidAppear(_ animated: Bool) {
-        let storyboard = UIStoryboard(name: "LoginStoryBoard", bundle: nil)
-        let controller = storyboard.instantiateViewController(identifier: "loginVC")
-        present(controller, animated: true, completion: nil)
-    }
 
 }
 
