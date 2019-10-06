a69df72bc46a1a619572b5eb023b2256f1f20244
shashankprabhakar <shashank.dwayne@gmail.com>
2019-10-06 20:47:57 +0100

    password match and password image change - done

--- EventSource/Controller/RegisterVC.swift
+++ EventSource/Controller/RegisterVC.swift
@@ -27,8 +27,36 @@ class RegisterVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        passwordTxt.addTarget(self, action: #selector(textFieldDidChange(_:)), for: UIControl.Event.editingChanged)
+        confirmPasswordTxt.addTarget(self, action: #selector(textFieldDidChange(_:)), for: UIControl.Event.editingChanged)
 
         // Do any additional setup after loading the view.
+    }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+    
+        guard let passTxt = passwordTxt.text else { return }
+        
+        if textField == confirmPasswordTxt {
+            passCheckImg.isHidden = false
+            confirmPassCheckImg.isHidden = false
+        } else {
+            if passTxt.isEmpty {
+                passCheckImg.isHidden = true
+                confirmPassCheckImg.isHidden = true
+                confirmPasswordTxt.text = ""
+            }
+        }
+        
+        //if password match, the checkmark turns green
+        if passwordTxt.text == confirmPasswordTxt.text {
+            passCheckImg.image = UIImage(named: "green_check")
+            confirmPassCheckImg.image = UIImage(named: "green_check")
+        } else {
+            passCheckImg.image = UIImage(named: "red_check")
+            confirmPassCheckImg.image = UIImage(named: "red_check")
+        }
     }
     
 
---
+++ EventSource/Utils/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  EventSource
+//
+//  Created by Shashank Prabhakar on 06/10/2019.
+//  Copyright © 2019 Shashank Prabhakar. All rights reserved.
+//
+
+import Foundation
